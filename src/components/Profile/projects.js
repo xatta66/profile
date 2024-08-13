21154019d3e067f393c6d5f0f6bf0904ae916fa2
xatta66 <xatta62007@mail.ru>
2024-08-13 19:32:15 +0300

    add irby-project

--- src/components/Profile/projects.js
+++ src/components/Profile/projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './projects.css';
-import CookiesBackImage from '../../assets/img/CookiesBackImage.png'
+import CookiesBackImage from '../../assets/img/CookiesBackImage.png';
+import IrbyBackImage from '../../assets/img/IrbyBackImage.png';
 
 const Projects = React.forwardRef((props, ref) => (
         <div className="Profile" ref={ref}>
@@ -12,6 +13,14 @@ const Projects = React.forwardRef((props, ref) => (
                     <div className="CookiesUrl">    
                         <a href="https://xatta66.github.io/cookies/" target="blank"><button className="CookiesButton">Перейти на сайт</button></a>
                         <a href="https://github.com/xatta66/cookies/" target="blank"><button className="CookiesButton">Репозиторий</button></a>
+                    </div> 
+                </div>
+                <div className="Irby">
+                    <h1>IrBY</h1>
+                    <img alt="IrbyBackImage" src={IrbyBackImage} className="IrbyBackImage"/>
+                    <div className="IrbyUrl">    
+                        <a href="https://xatta66.github.io/irby-project/" target="blank"><button className="IrbyButton">Перейти на сайт</button></a>
+                        <a href="https://github.com/xatta66/irby-project" target="blank"><button className="IrbyButton">Репозиторий</button></a>
                     </div> 
                 </div>
             </div>
